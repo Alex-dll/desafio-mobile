cc69f4f0d530ddfb0cac03ffb839b7fbb2d8c012
alex-dll <alexsandrojr.303@gmail.com>
2022-10-10 12:45:11 -0300

    feat: :sparkles: Create TextContainer

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import Header from '../../components/Header';
 
-import Icon from '../../components/Icon';
+import Typography from '../../components/Typography';
 
-import { Container } from './styles';
+import { Container, TextContainer } from './styles';
 
 const Home: React.FC = () => {
   return (
     <Container>
       <Header />
-      <Icon icon="search" size={17} activeColor="black" />
+      <TextContainer>
+        <Typography color="black" fontStyle="h1">
+          Funcionários
+        </Typography>
+      </TextContainer>
     </Container>
   );
 };
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,3 +4,7 @@ export const Container = styled.View`
   flex: 1;
   width: 100%;
 `;
+
+export const TextContainer = styled.View`
+  padding: 20px;
+`;
