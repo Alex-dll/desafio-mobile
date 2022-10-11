539b3b29c2dc7aa636f6e4d9f9b081a909d4981d
alex-dll <alexsandrojr.303@gmail.com>
2022-10-11 16:39:08 -0300

    feat: :lipstick: add padding to android app

--- src/App/styles.ts
+++ src/App/styles.ts
@@ -1,8 +1,14 @@
-import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.white.main};
   align-items: center;
   justify-content: center;
+  ${Platform.select({
+    android: css`
+      padding-top: 30px;
+    `,
+  })}
 `;
